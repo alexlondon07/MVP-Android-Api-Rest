cea4a1390179c1993cc7037d1c997912756221cf
Alexander Londoño Espejo <alexlondon07@gmail.com>
2017-10-29 21:49:30 -0500

    Se agrega Menu de navegacion, Home, Cursos, Profesores

--- app/src/main/java/io/github/alexlondon07/mvp_android_api_rest/App.java
+++ app/src/main/java/io/github/alexlondon07/mvp_android_api_rest/App.java
@@ -3,7 +3,7 @@ package io.github.alexlondon07.mvp_android_api_rest;
 import android.app.Application;
 
 /**
- * Created by alexlondon07 on 10/28/17.
+ * Created by alexlondon07 on 10/29/17.
  */
 
 public class App extends Application {
---
+++ app/src/main/java/io/github/alexlondon07/mvp_android_api_rest/view/activities/MainActivity.java
@@ -0,0 +1,48 @@
+package io.github.alexlondon07.mvp_android_api_rest.view.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import io.github.alexlondon07.mvp_android_api_rest.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_courses:
+                    Intent intent = new Intent(MainActivity.this, CourseActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.navigation_teachers:
+                    mTextMessage.setText(R.string.title_teachers);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
